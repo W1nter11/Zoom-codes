3167631173396eba3a76b7e783a47d41c1924f6b
W1nter11 <77439596+W1nter11@users.noreply.github.com>
2021-01-19 23:45:58 +0200

    Commented auto start

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import webbrowser
 
 
-os.startfile(r'C:\Users\User\AppData\Roaming\Zoom\bin\Zoom.exe')
+#os.startfile(r'C:\Users\User\AppData\Roaming\Zoom\bin\Zoom.exe')
 
 
 print("Предмет")
