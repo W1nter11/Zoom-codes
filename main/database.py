fccafc1882f61211636e1fd37e9fa0938f75ad82
W1nter11 <77439596+W1nter11@users.noreply.github.com>
2023-07-04 17:49:52 +0300

    DB connection
    Connected db with python functions

--- main/database.py
+++ main/database.py
@@ -1,45 +1,45 @@
 import sqlite3
 
 def createdatabase():
-    freespace = [("", "", "", "")]
-    with sqlite3.connect('main/data.sqlite') as db:
+    #freespace = [("", "", "")]
+    with sqlite3.connect('data.sqlite') as db:
         cursor = db.cursor()
-        query = """ CREATE TABLE IF NOT EXISTS zoomcodes(id TEXT, lesson TEXT, code TEXT, password TEXT)  """
+        query = """ CREATE TABLE IF NOT EXISTS zoomcodes(lesson TEXT, code TEXT, password TEXT)  """
         cursor.execute(query)
-    with sqlite3.connect('main/data.sqlite') as db:
+        db.commit()
+        print("Nice")
+    '''with sqlite3.connect('data.sqlite') as db:
         cursor = db.cursor()
-        query = """ INSERT INTO zoomcodes(id, lesson, code, password) VALUES (?, ?, ?, ?); """
+        query = """ INSERT INTO zoomcodes(lesson, code, password) VALUES (?, ?, ?); """
         cursor.executemany(query, freespace)
         db.commit()
+        print("Nice")'''
 
 def addtodatabase():
     ing_l = (input('Which lesson you want to add? '))
     ing_c = (input('Conferation code :  '))
     ing_p = (input('Conferation password : '))
-    with sqlite3.connect('main/data.sqlite') as db:
+    input_data = [(ing_l, ing_c, ing_p )]
+    with sqlite3.connect('data.sqlite') as db:
         cursor = db.cursor()
-        cursor.execute("SELECT max(rowid) from zoomcodes")
-        ing_i = cursor.fetchone()[0]
-    input_data = [(ing_i, ing_l, ing_c, ing_p )]
-    with sqlite3.connect('main/data.sqlite') as db:
-        cursor = db.cursor()
-        query = """ INSERT INTO zoomcodes(id, lesson, code, password) VALUES (?, ?, ?, ?); """
+        query = (" INSERT INTO zoomcodes(lesson, code, password) VALUES (?, ?, ?)")
         cursor.executemany(query, input_data)
         db.commit()
-    print(ing_i)
-
+        print("Nice")
 def deletefromdatabase():
-    with sqlite3.connect('main/data.sqlite') as db:
+    with sqlite3.connect('data.sqlite') as db:
             cursor = db.cursor()
             query = """DELETE FROM zoomcodes"""
             cursor.execute(query)
             db.commit()
+            print("Nice")
 
 def droptable():
-    with sqlite3.connect('main/data.sqlite') as db:
+    with sqlite3.connect('data.sqlite') as db:
         cursor = db.cursor()
         query = """DROP TABLE zoomcodes"""
         cursor.execute(query)
         db.commit()
+        print("Nice")
 
 addtodatabase()
